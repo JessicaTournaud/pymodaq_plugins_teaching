a496cd958ccf612d6efd0c3156788e51666eb713
JessicaTournaud <jessica.tournaud@umontpellier.fr>
2026-06-18 09:05:14 +0200

    Beam Profiler

---
+++ src/pymodaq_plugins_teaching/daq_viewer_plugins/plugins_2D/daq_2Dviewer_BeamProfiler.py
@@ -0,0 +1,25 @@
+
+from pymodaq.control_modules.viewer_utility_classes import main
+from pymodaq_data import DataCalculated
+from pymodaq_plugins_mockexamples.daq_viewer_plugins.plugins_2D.daq_2Dviewer_BSCamera import DAQ_2DViewer_BSCamera
+import laserbeamsize as lbs
+import numpy as np
+
+class DAQ_2DViewer_BeamProfiler(DAQ_2DViewer_BSCamera):
+
+    def grab_data(self, Naverage=1, **kwargs):
+        dte = self.average_data(Naverage)
+        data_array_2D = dte.get_data_from_name('Mock2D_0')[0]
+
+        x, y, d_major, d_minor, phi = lbs.beam_size(data_array_2D)
+        dte_0D_size = DataCalculated('BeamSize', data=[np.atleast_1d(d_major),
+                                                   np.atleast_1d(d_minor)], labels = ['Dminor','Dmajor'])
+
+        dte_0D_position = DataCalculated('BeamPosition', data=[np.atleast_1d(x), np.atleast_1d(y)], labels = ['X','Y'])
+
+        dte.append(dte_0D_position)
+        dte.append(dte_0D_size)
+        return self.dte_signal.emit(dte)
+
+if __name__ == '__main__':
+    main(__file__)
